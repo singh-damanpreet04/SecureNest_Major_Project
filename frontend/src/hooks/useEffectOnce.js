1c17c9bcc4d7ca3a401c47ee4d92d155e673f61d
singh-damanpreet04 <singh04damanpreet@gmail.com>
2025-12-09 06:35:58 +0530

    Delete frontend directory

--- frontend/src/hooks/useEffectOnce.js
+++
@@ -1,8 +0,0 @@
-import { useEffect } from 'react';
-
-export const useEffectOnce = (effect) => {
-  useEffect(() => {
-    effect();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-};
